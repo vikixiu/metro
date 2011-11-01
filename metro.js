445be7578c7c2865846726d3f4d6e5e6073b7ebc
viki xiu <vicky.wu1220@gmail.com>
2011-11-01 15:50:54 +0800

    -,-

--- metro.js
+++ metro.js
@@ -1,3 +1,92 @@
+function drawStation(line,x,y,name){
+	//var lineContext = document.getElementById(line).getContext('2d');
+		
+		line.save();
+		line.beginPath();
+		line.arc(x,y,6,0,Math.PI*2,true);
+		line.closePath();
+		line.fillStyle='#fff';
+		line.fill();
+		line.lineWidth=2;
+		line.stroke();
+		line.restore();
+		
+}
+
+function setStationInfo(lineCanvas,name,x,y){
+	//var $div = $('#' + lineCanvas).parent('div');
+	var $div = $('#stations');
+	var Info = '<div>' + name + '</div>';	
+	var	stationName = '<div class="stationName" style="left:' + (x- name.length*6) + 'px; top:' + (y-24) + 'px">' + name +'</div>',
+		stationInfo = '<div class="stationInfo" style="left:' + (x-6) + 'px; top:' + (y-6) + 'px">' + Info +'</div>';
+	$div.append(stationName,stationInfo);
+}
+
+function drawLine(lineCanvas,metroObj){
+
+	var lineContext = document.getElementById(lineCanvas).getContext('2d'),
+	position,P;
+	
+	lineContext.strokeStyle= metroObj.color;
+	lineContext.fillStyle= metroObj.color;
+	
+	
+	for(var i in metroObj.station){
+		position = metroObj.station[i].position;
+		var j= i-0+1;
+		
+		if(metroObj.station[j]){
+			var positionNext = metroObj.station[j].position;
+			lineContext.beginPath();
+			
+			if(metroObj.station[i].rc == true){
+				lineContext.moveTo(positionNext.x,positionNext.y);
+			}else{
+				lineContext.save();
+				lineContext.arc(position.x,position.y,0.2,0,Math.PI*2,true);
+				lineContext.fill();
+				lineContext.restore();
+			
+				lineContext.moveTo(position.x,position.y);}
+			
+			if(metroObj.station[j].rc == true){
+			
+				var n = j-0+1;
+				var curve = metroObj.station[n].position;
+				
+				lineContext.quadraticCurveTo(positionNext.x,positionNext.y,curve.x,curve.y);
+				//lineContext.moveTo(curve.x,curve.y);
+				//console.log(positionNext);			
+				//console.log(curve);			
+			}else{
+				//lineContext.quadraticCurveTo(1200,500,positionNext.x,positionNext.y);
+				lineContext.lineTo(positionNext.x,positionNext.y);
+			}
+			
+			
+			//lineContext.lineTo(positionNext.x,positionNext.y);
+			
+			lineContext.save();
+			lineContext.lineWidth=7;
+			lineContext.stroke();
+			lineContext.restore();
+		}
+		//drawStation(lineContext,position.x,position.y,metroObj.station[i].name);
+		if(metroObj.station[i].st == true){
+			drawStation(lineContext,position.x,position.y,metroObj.station[i].name);
+			}else{
+			
+			}
+		if(metroObj.station[i].slb == true){	
+			setStationInfo(lineCanvas,metroObj.station[i].name,position.x,position.y);
+		}	
+	}
+}
+
+
+
+$(document).ready(function(){
+
 var sw= [],stations = [],metroSH = {},x=0,y=0;
 /*680,680*/
 $.ajax({
@@ -22,6 +111,16 @@ $.ajax({
 		//console.log(sw.join(','));
 		metroSH = eval('({' + sw.join(',') + '})');
 		console.log(metroSH);
+		
+		for(var line in metroSH){
+			$('#lines').append('<canvas id="'+ metroSH[line].id + '"  width="2400" height="2400"></canvas>');
+			drawLine(metroSH[line].id,metroSH[line]);
+		}
+
 	}
 });
 
+	
+		
+	$('.stationInfo').hover(function(){$(this).children('div').fadeIn()},function(){$(this).children('div').fadeOut()});
+})	
